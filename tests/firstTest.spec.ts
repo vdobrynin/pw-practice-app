f530b8afdc0147dd2471b95bfe9d472b49b2c080
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-01-30 22:00:34 -0800

    test_suites

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,18 +1,35 @@
 import { test } from '@playwright/test'
 
 // test.beforeAll(() => {  //usually for DB
-// })
+// }) 
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
 })
 
-test('the first test', async ({ page }) => {
-    await page.getByText('Form Layouts').click()
+test.describe('suite1', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Charts').click()
+    })
+    test('the first test', async ({ page }) => {
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('navigate to datepicker page1', async ({ page }) => {
+        await page.getByText('Datepicker').click()
+    })
 })
 
-test('navigate to datepicker page', async ({ page }) => {
-    await page.getByText('Datepicker').click()
-})
+test.describe('suite2', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Forms').click()
+    })
 
+    test('the first test', async ({ page }) => {
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('navigate to datepicker page2', async ({ page }) => {
+        await page.getByText('Datepicker').click()
+    })
+})
