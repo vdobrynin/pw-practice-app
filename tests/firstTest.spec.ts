c5c99e1aee5ba240945550939375d85a7effa896
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-01-30 21:51:59 -0800

    datepicker

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,18 @@
 import { test } from '@playwright/test'
 
-test('the first test', async ({ page }) => {
+// test.beforeAll(() => {  //usually for DB
+// })
+
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
     await page.getByText('Forms').click()
+})
+
+test('the first test', async ({ page }) => {
     await page.getByText('Form Layouts').click()
 })
+
+test('navigate to datepicker page', async ({ page }) => {
+    await page.getByText('Datepicker').click()
+})
+
