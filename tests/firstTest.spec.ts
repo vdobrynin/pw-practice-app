e5277c15b8f200439e79a511c70f3bae49558510
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-02-01 23:15:27 -0800

    extracting values

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -95,3 +95,24 @@ test('reusing the locators', async ({ page }) => {
 
     await expect(emailField).toHaveValue('test@test.com') // assertion
 })
+
+test('extracting values', async ({ page }) => {
+    //single text value
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic form" })
+    const buttonText = await basicForm.locator('button').textContent()
+
+    expect(buttonText).toEqual('Submit')
+
+    //all text values
+    const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonsLabels).toContain("Option 1")
+
+    //input value
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect(placeholderValue).toEqual('Email')
+}) 
