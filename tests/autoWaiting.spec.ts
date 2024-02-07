9d0d7a69220aec4d912561aa1a357b164a9278eb
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-02-06 23:16:55 -0800

    timeouts

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test'
-import { stat } from 'original-fs'
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
     await page.goto('http://uitestingplayground.com/ajax')
     await page.getByText('Button Triggering AJAX Request').click()
     // await page.getByText('Form Layouts').click()
+    testInfo.setTimeout(testInfo.timeout + 2000) //the way to override timeout
 })
 
 test('auto waiting', async ({ page }) => {
@@ -30,11 +30,18 @@ test('alternative waits', async ({ page }) => {
     // await page.waitForSelector('.bg-success')
 
     // #2 wait for particular response
-    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+    await page.waitForResponse('http://uitestingplayground.com/ajaxdata', { timeout: 20000 })
 
     // #3 wait for network calls to be completed ('NOT RECOMMENDED')
-    await page.waitForLoadState('networkidle')
+    // await page.waitForLoadState('networkidle')
 
-    const text = await successButton.allTextContents()
+    const text = await successButton.textContent()
     expect(text).toEqual('Data loaded with AJAX get request.')
+})
+
+test('timeouts', async ({ page }) => {
+    // test.setTimeout(10000)
+    test.slow()
+    const successButton = page.locator('.bg-success')
+    await successButton.click()
 })
