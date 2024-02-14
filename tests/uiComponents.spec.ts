7d3a50c06c5ab5b061933b4e070114456111b6b0
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-02-13 22:45:31 -0800

    radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -27,4 +27,26 @@ test.describe('Form Layouts page', () => {
         expect(inputValue).toEqual('test2@test.com')
         await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
     })
+
+    test('radio buttons', async ({ page }) => {
+        const usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid" })
+
+        // await usingTheGridForm.getByLabel('Option 1')
+        //     .check({ force: true })         //to click use {force: true} --> cause it's invisible  
+        await usingTheGridForm.getByRole('radio', { name: "Option 1" })
+            .check({ force: true })
+
+        const radioStatus = await usingTheGridForm.getByRole('radio', { name: "Option 1" })
+            .isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', { name: "Option 1" }))
+            .toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', { name: "Option 2" })
+            .check({ force: true })
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 1" }).isChecked())
+            .toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 2" }).isChecked())
+            .toBeTruthy()
+    })
 })
