2243186baf519fd0d958873aa187e3cbadc8a262
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-02-14 22:14:48 -0800

    checkboxes, list & dropdowns, tooltips & dialog boxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test'
+import { dialog } from 'electron'
+import { filter } from 'rxjs/operators'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -49,4 +51,81 @@ test.describe('Form Layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', { name: "Option 2" }).isChecked())
             .toBeTruthy()
     })
+})
+
+test('checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', { name: "Hide on click" }).uncheck({ force: true })
+    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" }).check({ force: true })
+
+    const allBoxes = page.getByRole('checkbox')
+    // for (const box of await allBoxes.all()) {
+    //     await box.check({ force: true })
+    //     expect(await box.isChecked()).toBeTruthy() // to check all boxes
+    // }
+
+    for (const box of await allBoxes.all()) {
+        await box.uncheck({ force: true })
+        expect(await box.isChecked()).toBeFalsy() // to uncheck all boxes
+    }
+})
+
+test('lists and dropouts', async ({ page }) => {
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    page.getByRole('list')      // when the list has UL tag
+    page.getByRole('listitem')  // when the list has LI tag
+
+    // const optionList = page.getByRole('list').locator('nb-option')
+    const optionList = page.locator('nb-option-list nb-option')     // teacher prefer!
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"]) // check to all
+    await optionList.filter({ hasText: "Cosmic" }).click()  // cosmic only
+
+    const header = page.locator('nb-layout-header')         // check the header color
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+
+    await dropDownMenu.click()          // to open dropdown
+    for (const color in colors) {
+        await optionList.filter({ hasText: color }).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if (color != "Corporate") {
+            await dropDownMenu.click()      // to re-open dropdown
+        }
+    }
+})
+
+test('tooltips', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    const toolTipCard = page.locator('nb-card', { hasText: 'Tooltip Placements' })
+    await toolTipCard.getByRole('button', { name: 'Top' }).hover()
+
+    page.getByRole('tooltip')           // this way will work if you have a role tooltip created
+    const tooltip = await page.locator('nb-tooltip').textContent()
+    expect(tooltip).toEqual('This is a tooltip')
+})
+
+test('dialog boxes', async ({ page }) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+
+    await page.getByRole('table').locator('tr', { hasText: 'mdo@gmail.com' })
+        .locator('.nb-trash').click()
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
 })
