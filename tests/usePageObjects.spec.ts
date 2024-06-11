433bd463a88799f8913918f2e90120096c932406
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-06-10 21:19:25 -0700

    test refactor

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -38,3 +38,10 @@ test('parametrized methods @smoke', async ({ page }) => {
     // await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
     // await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
 })
+
+test.only('testing with argos ci', async ({ page }) => {
+
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datepickerPage()
+})
