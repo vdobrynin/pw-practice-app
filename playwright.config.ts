cde0ae30f5274e566d235f12f8556a4cee1d591f
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-06-10 23:35:20 -0700

    fixing config

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,18 +17,18 @@ export default defineConfig<TestOptions>({
     timeout: 2000,
     toMatchSnapshot: { maxDiffPixels: 50 } //test will not fail setup it if test not stable
   },
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: false,
+  // testDir: './tests',
+  // /* Run tests in files in parallel */
+  // fullyParallel: false,
 
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,            //--> make 1 retries ***
+  // /* Fail the build on CI if you accidentally left test.only in the source code. */
+  // forbidOnly: !!process.env.CI,
+  // /* Retry on CI only */
+  // retries: process.env.CI ? 2 : 1,            //--> make 1 retries ***
 
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  // workers: process.env.CI ? 1 : 5,          //--->this will run 5 workers, not 4 *** 
+  // /* Opt out of parallel tests on CI. */
+  // workers: process.env.CI ? 1 : undefined,
+  // // workers: process.env.CI ? 1 : 5,          //--->this will run 5 workers, not 4 *** 
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
