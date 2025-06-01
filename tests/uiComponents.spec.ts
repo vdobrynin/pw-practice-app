b334bc1b0e7e0e8c7b39c12b3df056cca2838e19
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-01 00:00:19 -0700

    #35 checkboxes - review

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -68,22 +68,25 @@ test.describe('Form Layouts page', () => {
 })
 
 test('checkboxes', async ({ page }) => {
-
     await page.getByText('Modal & Overlays').click()
     await page.getByText('Toastr').click()
 
-    await page.getByRole('checkbox', { name: "Hide on click" }).uncheck({ force: true })
-    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" }).check({ force: true })
+    await page.getByRole('checkbox', { name: "Hide on click" })
+        .uncheck({ force: true })
+    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" })
+        .check({ force: true })
 
     const allBoxes = page.getByRole('checkbox')
-    // for (const box of await allBoxes.all()) {
-    //     await box.check({ force: true })
-    //     expect(await box.isChecked()).toBeTruthy() // to check all boxes
-    // }
+    for (const box of await allBoxes.all()) {
+        await box.check({ force: true })
+        expect(await box.isChecked())
+            .toBeTruthy() // to check all boxes
+    }
 
     for (const box of await allBoxes.all()) {
         await box.uncheck({ force: true })
-        expect(await box.isChecked()).toBeFalsy() // to uncheck all boxes
+        expect(await box.isChecked())
+            .toBeFalsy() // to uncheck all boxes
     }
 })
 
