030112a2d2c1f7c6a90ba79601d94af2dd97afce
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-02 16:43:00 -0700

    #38 dialog boxes - review

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -136,17 +136,23 @@ test('tooltips', async ({ page }) => {
 })
 
 test('dialog boxes', async ({ page }) => {
-
     await page.getByText('Tables & Data').click()
     await page.getByText('Smart Table').click()
 
     page.on('dialog', dialog => {
-        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        expect(dialog.message())
+            .toEqual('Are you sure you want to delete?')
         dialog.accept()
     })
 
-    await page.getByRole('table').locator('tr', { hasText: 'mdo@gmail.com' }).locator('.nb-trash').click()
-    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+    await page.getByRole('table')
+        .locator('tr', { hasText: 'mdo@gmail.com' })
+        .locator('.nb-trash')
+        .click()
+    await expect(page.locator('table tr')
+        .first())
+        .not
+        .toHaveText('mdo@gmail.com')
 })
 
 test('web tables', async ({ page }) => {
