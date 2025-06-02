8b65a70729d8fe77644e8fa96094e6df705d7c32
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-02 16:29:27 -0700

    #37 tooltips - review

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -124,14 +124,13 @@ test('lists and dropouts', async ({ page }) => {
 })
 
 test('tooltips', async ({ page }) => {
-
     await page.getByText('Modal & Overlays').click()
     await page.getByText('Tooltip').click()
 
     const toolTipCard = page.locator('nb-card', { hasText: 'Tooltip Placements' })
     await toolTipCard.getByRole('button', { name: 'Top' }).hover()
 
-    page.getByRole('tooltip')           // this way will work if you have a role tooltip created
+    // page.getByRole('tooltip')           // this way will work if you have a role tooltip created
     const tooltip = await page.locator('nb-tooltip').textContent()
     expect(tooltip).toEqual('This is a tooltip')
 })
