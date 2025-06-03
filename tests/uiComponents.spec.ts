4cb0479c9299e3723480ed54c5d5791975de5228
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-02 17:51:29 -0700

    #3 web tables part 1.1 - review

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -156,46 +156,58 @@ test('dialog boxes', async ({ page }) => {
 })
 
 test('web tables', async ({ page }) => {
-
-    await page.getByText('Tables & Data').click()
-    await page.getByText('Smart Table').click()
-
+    await page.getByText('Tables & Data')
+        .click()
+    await page.getByText('Smart Table')
+        .click()
+    
     // 1st get the row by any test in this row
-    const targetRow = page.getByRole('row', { name: 'twitter@outlook.com' })
-    await targetRow.locator('.nb-edit').click()
-    await page.locator('input-editor').getByPlaceholder('Age').clear()
-    await page.locator('input-editor').getByPlaceholder('Age').fill('35')
-    await page.locator('.nb-checkmark').click()
+    const targetRow = page
+        .getByRole('row', { name: 'twitter@outlook.com' })
+    await targetRow
+        .locator('.nb-edit')
+        .click()
+    await page
+        .locator('input-editor')
+        .getByPlaceholder('Age')
+        .clear()
+    await page
+        .locator('input-editor')
+        .getByPlaceholder('Age')
+        .fill('35')
+    await page
+        .locator('.nb-checkmark')
+        .click()
+    
+    // // 2nd get the row based on the value in the specific column
+    // await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+    // const targetRowById = page.getByRole('row', { name: '11' }).filter({ has: page.locator('td').nth(1).getByText('11') })
+    // await targetRowById.locator('.nb-edit').click()
 
-    // 2nd get the row based on the value in the specific column
-    await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
-    const targetRowById = page.getByRole('row', { name: '11' }).filter({ has: page.locator('td').nth(1).getByText('11') })
-    await targetRowById.locator('.nb-edit').click()
+    // await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+    // await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+    // await page.locator('.nb-checkmark').click()
+    // await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
 
-    await page.locator('input-editor').getByPlaceholder('E-mail').clear()
-    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
-    await page.locator('.nb-checkmark').click()
-    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
+    // // 3d test filter of the table
+    // const ages = ['20', '30', '40', '200']
 
-    // 3d test filter of the table
-    const ages = ['20', '30', '40', '200']
+    // for (let age of ages) {
+    //     await page.locator('input-filter').getByPlaceholder('Age').clear()
+    //     await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+    //     await page.waitForTimeout(300)
 
-    for (let age of ages) {
-        await page.locator('input-filter').getByPlaceholder('Age').clear()
-        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
-        await page.waitForTimeout(300)
+    //     const ageRows = page.locator('tbody tr')
+    //     for (let row of await ageRows.all()) {
+    //         const cellValue = await row.locator('td').last().textContent()
 
-        const ageRows = page.locator('tbody tr')
-        for (let row of await ageRows.all()) {
-            const cellValue = await row.locator('td').last().textContent()
-
-            if (age == '200') {
-                expect(await page.getByRole('table').textContent()).toContain('No data found')
-            } else {
-                expect(cellValue).toEqual(age)
-            }
-        }
-    }
+    //         if (age == '200') {
+    //             expect(await page.getByRole('table').textContent()).toContain('No data found')
+    //         } else {
+    //             expect(cellValue).toEqual(age)
+    //         }
+    //     }
+    // }
 })
 
 test('datepicker', async ({ page }) => {
