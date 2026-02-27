c3a29823ff5777b3948c19044e5419123a2d76fe
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2026-02-26 16:49:20 -0800

    review & refactor - #25 user-facing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -38,14 +38,14 @@ test('Locator syntax rules', async ({ page }) => {
     page.locator(':text-is("Using the Grid)')
 })
 
-test('User facing locators', async ({ page }) => {
-    await page.getByRole('textbox', { name: "Email" }).first().click()     // #25
+test('user facing locators', async ({ page }) => {
+    await page.getByRole('textbox', { name: "Email" }).first().click()     // #25 user-facing locators
     await page.getByRole('button', { name: "Sign in" }).first().click()
 
     await page.getByLabel('Email').first().click()
     await page.getByPlaceholder('Jane Doe').click()
-    await page.getByText('Using the Grid').click()
-    await page.getByTestId('SignIn').click()        // --> custom id by me (change in html - source code ***)
+    await page.getByText('Using the Grid').click() 
+    await page.getByTestId('SignIn').click() //--> custom id by me(add in html - in source code *** 'data-testid="SignIn'") 'data-testid' reserve word
     await page.getByTitle('IoT Dashboard').click()
 })
 
